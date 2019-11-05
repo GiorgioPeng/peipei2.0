b927bbd80f7039d808aee31104d7b87d449f4178
Giorgio <GiorgioPeng@github.com>
2019-11-05 21:18:41 +0800

    developing
    初步完成了页面架构,基本完成了登录页面

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,45 +1,110 @@
 <template>
   <div class="Login">
-    <div class="classForInputData">
-      <input type="text" :value="school" />
-      <input type="text" :value="name" />
-      <input type="text" :value="studentId" />
+    <div class="logo">
+      <img src="../../public/Login/logo.png" />
     </div>
-    <input type="button" value="登录" />
-    <input type="checkbox" id="agree" />
-    <label for="agree">{{agree}}</label>
+    <div class="classForInputData">
+      <input type="text" v-model.lazy="school" placeholder="学校" />
+      <input type="text" v-model.lazy="name" placeholder="姓名" />
+      <input type="text" v-model.lazy="studentId" placeholder="学号" />
+    </div>
+    <div class="loginButton"></div>
+    <div class="agree">
+      <input type="checkbox" id="agree" />
+      <label for="agree">{{agree}}</label>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop } from "vue-property-decorator";
+import { Component, Vue, Prop, Watch } from "vue-property-decorator";
 
 @Component({
   name: "Login"
 })
 export default class extends Vue {
-  private school: string = "学校";
-  private name: string = "姓名";
-  private studentId: string = "学号";
+  private school: string = "";
+  private name: string = "";
+  private studentId: string = "";
   private agree: string = "同意佩佩读取您的个人信息";
 }
 </script>
 
 <style lang="stylus">
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.Login {
+  width: 100%;
+  height: 100%;
+  background-image: url('../../public/Login/background.png');
+  background-size: 100% 100%;
+  background-position: top left;
+  background-repeat: no-repeat;
+}
+
+.logo {
+  clear: both;
+  display: block;
+  margin: auto;
+  padding-top: 19vh;
+  width: 38.4vw;
+  height: 38.4vw;
+}
+
+.logo img {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+}
+
 .classForInputData {
   display: flex;
   flex-direction: column;
   width: 70vw;
-  margin: 100px auto;
+  margin: 10vh auto;
+  margin-bottom: 0;
   height: 200px;
   justify-content: space-around;
 }
 
 .classForInputData input {
   width: 100%;
-  height: 30px;
-  border-radius: 10%;
-  border: thin soild rgba(77, 137, 138, 0.7);
+  height: 22%;
+  border: none;
   outline: none;
+  background-image: url('../../public/Login/input.png');
+  background-size: 100% 100%;
+  text-align: center;
+  background-color: rgba(0, 0, 0, 0);
+  color: rgba(77, 137, 138, 0.7);
+}
+
+.loginButton {
+  margin: 3vh auto;
+  background-image: url('../../public/Login/loginButton.png');
+  background-position: 0 0;
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  width: 47vw;
+  height: 11vw;
+}
+
+.agree {
+  display: flex;
+  justify-content: center;
+}
+
+input['type="checkbox"'] {
+  outline: none;
+  background-color: white;
+  border: 5px solid #5bbfbf;
+}
+
+label {
+  font-size: 1vw;
+  color: rgba(77, 137, 138, 0.7);
 }
 </style>
