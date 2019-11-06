0bcd9801b7ad2d8c88d31614cd1bec19963181da
Giorgio <GiorgioPeng@github.com>
2019-11-06 22:39:24 +0800

    developing

--- src/components/entertainment/EntertainmentPage1.vue
+++ src/components/entertainment/EntertainmentPage1.vue
@@ -1,7 +1,45 @@
 <template>
-  <div></div>
+  <div>
+    <div class="gamePhoto">
+      <div class="biggerGamePhoto"></div>
+      <div class="smallerGamePhoto"></div>
+    </div>
+    <div class="frame">
+      <div class="biggerFrame"></div>
+      <div class="smallerFrame"></div>
+    </div>
+  </div>
 </template>
-<script>
-export default {};
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component({
+  name: "Entertainment"
+})
+export default class Entertainment extends Vue {}
 </script>
-<style lang="stylus"></style>
+<style lang="stylus">
+.gamePhoto {
+  width: 80vw;
+  height: 50vw;
+}
+
+.biggerGamePhoto {
+  background-image: url('../../../public/entertainment/firstPage/PUBG.png');
+  background-size: 100% 100%;
+  width: 100%;
+  height: 100%;
+}
+
+.frame {
+  width: 80vw;
+  height: 50vw;
+}
+
+.biggerFrame {
+  background-image: url('../../../public/entertainment/firstPage/frame1.png');
+  background-size: 100% 100%;
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/components/sleep/SleepPage1.vue
+++ src/components/sleep/SleepPage1.vue
@@ -1,7 +1,41 @@
 <template>
-  <div></div>
+  <div>
+    <div class="line"></div>
+    <div>
+      <div class="settingHint"></div>
+      <div class="day"></div>
+      <div class="night"></div>
+    </div>
+    <div class="weightPart">
+      <div class="weightHint"></div>
+      <div class="weight"></div>
+    </div>
+  </div>
 </template>
-<script>
-export default {};
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component({
+  name: "Sleep"
+})
+export default class Sleep extends Vue {}
 </script>
-<style lang="stylus"></style>
+<style lang="stylus">
+.day {
+  background-image: url('../../../public/sleep/firstPage/day.png');
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  background-position: left top;
+  width: 200px;
+  height: 200px;
+}
+
+.night {
+  background-image: url('../../../public/sleep/firstPage/night.png');
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  background-position: left top;
+  width: 200px;
+  height: 200px;
+}
+</style>
