743e7a11677a1440340661978a1bdb781de46e09
Yuedeng <944389333@qq.com>
2019-11-28 20:01:42 +0800

    wyr

--- backEnd/main.go
+++ backEnd/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"github.com/kataras/iris/v12"
+	"log"
 
 	"peipei2/router"
 )
 
 func main() {
-
+	log.Println("starting server")
 	app := router.NewApp()
 	err := app.Run(iris.Addr(":8000"), iris.WithoutServerError(iris.ErrServerClosed))
 	if err != nil {
