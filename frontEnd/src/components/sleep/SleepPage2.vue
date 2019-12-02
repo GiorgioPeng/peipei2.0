6553749b430295bbb3ffd5d01c0885b1dbd3a426
pengzouzhang <84867579@qq.com>
2019-12-02 16:49:43 +0800

    sleep page2

--- frontEnd/src/components/sleep/SleepPage2.vue
+++ frontEnd/src/components/sleep/SleepPage2.vue
@@ -7,14 +7,27 @@
     <div style="display:flex;justify-content:center;align-items:center">
       <div class="back"></div>
       <div class="time">
-        <div class="mainDisplay"></div>
+        <div class="mainDisplay">
+          <div>{{timeFrame=='AM'?am[0]:pm[0]}}</div>
+          <div>{{timeFrame=='AM'?am[1]:pm[1]}}</div>
+          <div></div>
+          <div>{{timeFrame=='AM'?am[2]:pm[2]}}</div>
+          <div>{{timeFrame=='AM'?am[3]:pm[3]}}</div>
+          <div>{{timeFrame=='AM'?am[4]:pm[4]}}</div>
+          <div>{{timeFrame=='AM'?am[5]:pm[5]}}</div>
+          <div></div>
+          <div>{{timeFrame=='AM'?am[6]:pm[6]}}</div>
+          <div>{{timeFrame=='AM'?am[7]:pm[7]}}</div>
+        </div>
         <div class="detailChoice">
           <div class="amOfPm">{{this.timeFrame}}</div>
           <div class="timeChoice">
             <div v-if="timeFrame=='AM'">
-              <div v-for="am in amChoiceDetail" :key="am">{{am}}</div>
+              <div v-for="am in amChoiceDetail" :key="am" @click="chooseTime(am,'am')">{{am}}</div>
             </div>
-            <div v-else v-for="pm in pmChoiceDetail" :key="pm">{{pm}}</div>
+            <div v-else>
+              <div v-for="pm in pmChoiceDetail" :key="pm" @click="chooseTime(pm,'pm')">{{pm}}</div>
+            </div>
           </div>
         </div>
       </div>
@@ -24,13 +37,18 @@
 </template>
 <script lang='ts'>
 import { Vue, Component } from "vue-property-decorator";
+@Component({
+  name: "SleepPage2"
+})
 export default class SleepPage2 extends Vue {
+  private am: string[] = ["0", "8", "0", "1", "0", "9", "0", "0"];
+  private pm: string[] = ["2", "2", "0", "1", "2", "3", "0", "0"];
   private timeFrame: string = "AM";
   private amChoiceDetail: string[] = [
-    "7:00之前",
-    "7:01-8:00",
-    "8:01-9:00",
-    "9:01-10:00",
+    "07:00之前",
+    "07:01-08:00",
+    "08:01-09:00",
+    "09:01-10:00",
     "10:01之后"
   ];
   private pmChoiceDetail: string[] = [
@@ -40,6 +58,49 @@ export default class SleepPage2 extends Vue {
     "23:01-24:00",
     "24:01之后"
   ];
+  public chooseTime(time: string, time2: string) {
+    let i = 0;
+    let tempTime = time;
+    if (time2 == "am") {
+      if (time.indexOf("之前") != -1) {
+        time = time.slice(0, time.length - 2);
+        time = "XX:XX-" + time;
+      } else if (time.indexOf("之后") != -1) {
+        time = time.slice(0, time.length - 2);
+        time += "-XX:XX";
+      }
+      time.split("-").map(res => {
+        res.split(":").map(res => {
+          res.split("").map(res => (this.am[i++] = res));
+        });
+      }); 
+      for(let k in this.amChoiceDetail){
+        if(this.amChoiceDetail[k]==tempTime)
+          this.amChoiceDetail[k] = this.amChoiceDetail[2];
+      }
+      this.amChoiceDetail[2] = tempTime;
+
+      this.$forceUpdate();
+      console.log(this.am);
+    }
+    if (time2 == "pm") {
+      if (time.indexOf("之前") != -1) {
+        time = time.slice(0, time.length - 2);
+        time = "XX:XX-" + time;
+      } else if (time.indexOf("之后") != -1) {
+        time = time.slice(0, time.length - 2);
+        time += "-XX:XX";
+      }
+      time.split("-").map(res => {
+        res.split(":").map(res => {
+          res.split("").map(res => (this.pm[i++] = res));
+        });
+      });
+
+      this.$forceUpdate();
+      console.log(this.pm);
+    }
+  }
 }
 </script>
 <style lang="stylus">
@@ -79,7 +140,7 @@ export default class SleepPage2 extends Vue {
     background-size: 100% 100%;
     height: 30px;
     width: 30px;
-    transform:translateX(10px);
+    transform: translateX(10px);
   }
 
   .time {
@@ -92,6 +153,31 @@ export default class SleepPage2 extends Vue {
       background-size: 100% 100%;
       width: 70%;
       height: 30%;
+      display: grid;
+      grid-template-columns: 45px 45px 22px 45px 45px;
+      grid-column-gap: 5px;
+      grid-row-gap: 10px;
+
+      div {
+        font-size: 60px;
+        font-weight: bold;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: white;
+        background-image: url('../../../public/sleep/secondPage/shadow.png');
+        background-size: 100% 50%;
+        background-position: center top;
+        background-repeat: no-repeat;
+      }
+
+      div:nth-child(3) {
+        background-image: none;
+      }
+
+      div:nth-child(8) {
+        background-image: none;
+      }
     }
 
     .detailChoice {
@@ -123,6 +209,14 @@ export default class SleepPage2 extends Vue {
           font-size: 30px;
           padding: 15px;
           color: #ceccd1;
+          text-align: center;
+
+          div:nth-child(3) {
+            color: #56bebf;
+            font-size: 35px;
+            background-image: url('../../../public/sleep/secondPage/greySlot.png');
+            background-size: 100% 100%;
+          }
         }
       }
     }
